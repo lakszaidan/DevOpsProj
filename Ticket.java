2109fe128beba70885e52425365614d71f38a55c
DESKTOP-JQ6TELU\Laksamana Zaidan <laksamanazaidan@gmail.com>
2021-01-26 14:32:21 +0700

    first commit

---
+++ Ticket.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Ticket {
+
+    public static void main(String[] args) {
+  
+      
+      Scanner input_bio=new Scanner (System.in);
+        System.out.println("Ticket Ta'imir Masjid push ketiga check otomatis");
+        System.out.println("----------------");
+     
+      
+        
+      
+      System.out.println("Nama = Ravi Haryo Baskoro & Laksamana Zaidan");
+      System.out.println("Nim = 1920010195");
+        System.out.println("Kelas = 3SC5");
+        System.out.println("Jurusan = Software Enginering");
+        System.out.println("\n");
+        System.out.println("--- Print Biodata seminar----");
+	System.out.println("----------------");
+	System.out.println("Undangan pengajian");
+	System.out.println("----------------");
+	System.out.println("jam = 04.00");
+	System.out.println("Tempat = Masjid UI");
+	System.out.println("Nomor Tempat Duduk = 001-002");
+	System.out.println("Tema Seminar = pentingnya ilmu agama dalam penerapan kehidupan");
+//        Scanner input= new Scanner(System.in);
+//
+//        int panjang, lebar, luas;
+//      
+//        System.out.println("PROGRAM JAVA MENGHITUNG LUAS PERSEGI PANJANG : ");
+//        System.out.println("-----------------------------------------------");
+//
+//        System.out.print("Masukan Panjang      : ");
+//        panjang=input.nextInt();
+//        System.out.print("Masukan Lebar        : ");
+//        lebar=input.nextInt();
+//        
+//        luas=panjang*lebar;
+//
+//        System.out.println("Luas Persegi Panjang : "+luas);
+    }
+    }
